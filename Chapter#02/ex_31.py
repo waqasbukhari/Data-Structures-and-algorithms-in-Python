7cdce3f8700ae53a7c3f44e65aa41c02737e19b6
Waqas Bukhari <waqas07_engr@yahoo.com>
2022-08-02 00:10:12 +0300

    some code.

--- Chapter#02/ex_31.py
+++ Chapter#02/ex_31.py
@@ -8,4 +8,14 @@ The changes are enclosed within comments specifying it to be part of C-2.27.
 from Progression import Progression
 
 class AbsProgression(Progression):
-    pass
+    def __init__(self, first=2, second=200):
+        super().__init__(first)
+        self._prev = second + first
+        
+    def _advance(self):
+        self._current, self._prev = abs(self._current - self._prev), self._current
+
+
+if __name__ == "__main__":    
+    p = AbsProgression(2,200)
+    p.print_progression(10)
