3493970d78dc8cf3e547d2360b63cf96e0bb0ac4
Waqas Bukhari <waqas07_engr@yahoo.com>
2022-08-01 09:00:16 +0300

    some code in R-exercies of chapter2.

---
+++ Chapter#02/ex_R_2.18_to_2.21.py
@@ -0,0 +1,24 @@
+from code_fragment_2_8_to_2_12 import FibbonacciProgression
+
+# R-2.18
+print('Fibonacci progression with start values 4 and 6: ')
+FibbonacciProgression(2, 2).print_progression(8)
+
+# R-2.19
+"""
+start = 0
+increment = 128 = 2^7
+target_level_to_reach = 2^63
+
+required_steps = 2^63 / 2^7 = 2^56 steps; each with an increment of 2^7 to reach a level of 2^63. 
+
+"""
+
+# R-2.20
+"""
+It would be at the cost of modularity and reduced independence of classes from each other.  
+""" 
+# R-2.21
+"""
+""" 
+
