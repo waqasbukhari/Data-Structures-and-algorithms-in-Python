a935f7e6602da0189a53207d5111517f9734c47f
Waqas Bukhari <wbukhari@beyond.ai>
2022-08-03 18:02:55 +0300

    chapter 4 on recursion done till exercises C.

--- Chapter#04/ex_20.py
+++ Chapter#04/ex_20.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-
-def even_first(seq, start=None, end=None):
+from random import shuffle
+def rearrange_around_k(seq, k, start=None, end=None):
     """
     in each recursive run.
     i) if len(seq) < 2, base case.  
     ii) if start >= end, base case. 
-    iii) iterate start until it reaches an odd value. If start does not reach an odd value, base case. 
-    iv) iterate end backwards until it reaches an even value. If end does not reach an even value, base case. 
+    iii) iterate start until it reaches a value (>k). If start does not reach such a value, base case. 
+    iv) iterate end backwards until it reaches a value (<=k). If end does not reach such a value, base case. 
     v) if the base case iii) and iv) is not reached, replace the contents of start and end and return a subsequent recursive call with appropriate start and end. 
     """
     # # base case
@@ -22,7 +22,7 @@ def even_first(seq, start=None, end=None):
         return seq
 
     for st_index in range(start, end+1): # iterate to find index with odd value. 
-        if seq[st_index] % 2 == 1:
+        if seq[st_index] > k :
             break
 
     if st_index == end: # base case iii) odd value is reached at end, => nothing to replace. 
@@ -31,7 +31,7 @@ def even_first(seq, start=None, end=None):
     start = st_index
 
     for en_index in range(end, start-1, -1): # iterate to find index with odd value. 
-        if seq[en_index] % 2 == 0:
+        if seq[en_index] < k:
             break
 
     if en_index == start: # base case iv) even value is reached at start, => nothing to replace. 
@@ -41,26 +41,29 @@ def even_first(seq, start=None, end=None):
 
     seq[start], seq[end] = seq[end], seq[start]
 
-    return even_first(seq, start=st_index+1, end=en_index-1)
+    return rearrange_around_k(seq, k, start=st_index+1, end=en_index-1)
     
 if __name__ == "__main__":
     print()
     a = list(range(1))
-    print(even_first(a))
+    print(rearrange_around_k(a, 5))
 
     print()
     a = list(range(2))
-    print(even_first(a))
+    print(rearrange_around_k(a, 5))
 
     print()
     a = list(range(100))
-    print(even_first(a))
+    shuffle(a)
+    print(rearrange_around_k(a, 25))
 
     print()
     a = [1,3,5,7,9,11,13,15]
-    print(even_first(a))
+    shuffle(a)
+    print(rearrange_around_k(a, 10))
 
     print()
     a = list(range(2,200,2))
-    print(even_first(a))
+    shuffle(a)
+    print(rearrange_around_k(a, 105))
 
