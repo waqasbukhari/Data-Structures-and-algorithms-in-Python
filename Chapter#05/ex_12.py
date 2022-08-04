f6bcf05c7a5ed0f9d85001761236934b257523cd
Waqas Bukhari <wbukhari@beyond.ai>
2022-08-04 15:59:36 +0300

    exercises in chapt 5

--- Chapter#05/ex_12.py
+++ Chapter#05/ex_12.py
@@ -1,9 +1,5 @@
-       
-if __name__ == '__main__':
+#!/usr/bin/env python3
 
-    arr = [list(range(4)),list(range(1,5)),list(range(2,6)),list(range(3,7))]
-
-    summation = 0
-    summation = sum([sum(inner_arr) for inner_arr in arr])
-    print(arr)
-    print(summation)
+"""
+Alreayd done in ex R-5.11
+"""
--- Chapter#05/ex_13.py
+++ Chapter#05/ex_13.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys # provides getsizeof function
 data = [ ]
 n = 26
