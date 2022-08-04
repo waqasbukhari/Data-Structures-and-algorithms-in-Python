3e72026b2d379e7b30242b1decd44c4836aa067d
Waqas Bukhari <waqas07_engr@yahoo.com>
2022-08-04 03:33:42 +0300

    making changes

--- Chapter#05/ex_06.py
+++ Chapter#05/ex_06.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python3
+#!/usr/bin/env python3
 
-def find_repeated(seq):
-    """
-    there are 1 to n-1 elements with exactly 1 repeated. 
-    summation over the entire sequence minus the summation from 1 to n-1 would reveal the repeated number. 
-    sum from 1 to n-1 is (n-1)(n-2)/2
-    """
-    n = len(seq)
-    sum_1_n_ = 0.5 * (n-1) * (n-2)
-    return int(sum(seq) - sum_1_n_)
-    
-if __name__ == "__main__":
-    a = list(range(22)) + [7]
-    print(find_repeated(a))
-    a = list(range(205)) + [25]
-    print(find_repeated(a))
-    a = list(range(105)) + [30]
-    print(find_repeated(a))
-    a = list(range(225)) + [75]
-    print(find_repeated(a))
-    
+"""
+Modification is made to method __getitem__ of DynamicArray class. 
+Changes are enclosed within comments, specifying ex R-5.6
+"""
