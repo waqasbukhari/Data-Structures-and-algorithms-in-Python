1613ef1ad9761e4ee8152b24fcae1783771a4a1c
Waqas Bukhari <waqas07_engr@yahoo.com>
2022-08-07 04:43:09 +0300

    some code changes in Chapter 6.

--- Chapter#06/Queue.py
+++ Chapter#06/Queue.py
@@ -1,7 +1,4 @@
-class Empty(Exception):
-    """ Error attempting to access element from empty container.
-        Note that Empty is a subclass of python's builtin Exception class"""
-    pass
+from Stack import Empty
 
 class ArrayQueue:
     DEFAULT_CAPACITY = 10
@@ -17,52 +14,62 @@ class ArrayQueue:
         iii)  len ~ returns length of the Stack.
 
     """
-
     def __init__(self):
-        self._data = [None] * ArrayQueue.DEFAULT_CAPACITY
-        self._size = 0 # Number of elements in the queue
-        self._front = 0 # index of the first element that is to be removed
-
+        self._capacity = ArrayQueue.DEFAULT_CAPACITY
+        self._data = [None] * self._capacity
+        self._size = 0
+        self._front = 0 # current index where element has to be pushed. 
+        
     def __len__(self):
         return self._size
-
+        
     def is_empty(self):
         return self._size == 0
-
-    def first(self):
-        if self.is_empty():
-            raise Empty('Queue is empty')
         
+    def first(self):
+        if self._size == 0:
+            raise Empty("Queue is empty. ")
+            
         return self._data[self._front]
-
+    
     def dequeue(self):
-        if self.is_empty():
-            raise Empty('Queue is empty')
-
-        answer = self._data[self._front]
-        self._data[self._front] = None # help garbage collection
+        if self._size == 0:
+            raise Empty("Queue is empty. ")
+            
+        front = self._data[self._front]
+        self._data[self._front] = None
+        # In case self._frong was at index len(self._data) -1 then adding 1 would make it len(self._data)
+        # which is beyond the indexes. so, to move it to 0, we use modulus operator. 
+        
+        self._front =  (self._front + 1) % self._capacity
         self._size -= 1
-        self._front = (self._front + 1) % len(self._data)
-
-        return answer
+        
+        return front
     
     def enqueue(self, e):
-        if self._size == len(self._data):
-            self._resize( 2*len(self._data))
-
-        index = (self._front + self._size) % len(self._data)
-        self._data[index] = e
+        if len(self) == self._capacity: 
+            self._resize(2*self._capacity)
+            
+        idx = (self._front + self._size) % len(self._data)
+        self._data[idx ] = e
         self._size += 1
-
-    def _resize(self, new_capacity):
-        B = [None] * new_capacity
-        for idx in range(self._size):
-            B[idx] = self._data[self._front]
-            self._front = (self._front + 1) % len(self._data)
-
-        self._data = B
+        
+    
+    def _resize(self, capacity):
+        
+    
+        B = [None] * capacity
+        for i in range(self._size): 
+            idx = (self._front + i ) % len(self)
+            B[i] = self._data[idx]
+            
         self._front = 0
-
+        self._data = B
+        self._capacity = capacity
+        
+        print(self._data    )
+        
+        
     
     
 if __name__ == '__main__':
@@ -71,14 +78,24 @@ if __name__ == '__main__':
     Q.enqueue(2)
     Q.enqueue(3)
     Q.enqueue(4)
+    Q.dequeue()
     Q.enqueue(5)
     Q.enqueue(6)
     Q.enqueue(7)
+    Q.dequeue()
     Q.enqueue(8)
     Q.enqueue(9)
+    Q.dequeue()
     Q.enqueue(10)
+    Q.enqueue(20)
+    Q.enqueue(30)
+    Q.enqueue(40)
+    print(len(Q))
+    print(Q._size)
+    print(Q._front)
     print(Q.dequeue( ))
     Q.enqueue(11)
+    #### resizing. 
     Q.enqueue(12)
     Q.enqueue(13)
     Q.enqueue(14)
@@ -92,3 +109,27 @@ if __name__ == '__main__':
     Q.enqueue(4)
     print(len(Q))
     print(Q.dequeue( ))
+    Q.enqueue(12)
+    Q.enqueue(13)
+    Q.enqueue(14)
+    Q.enqueue(15)
+    print(Q._front)
+    print(Q._size)
+    print(Q._data)
+    Q.enqueue(1)
+    Q.enqueue(2)
+    print(Q._front)
+    print(Q._size)
+    print(Q._data)
+    Q.enqueue(3)
+    print(Q._front)
+    print(Q._size)
+    print(Q._data)
+    print(ses)
+    Q.enqueue(4)
+    Q.enqueue(5)
+    Q.enqueue(6)
+    Q.enqueue(7)
+    print(Q._data)
+    print(Q.first())
+    print(len(Q))
--- Chapter#06/ex_11.py
+++ Chapter#06/ex_11.py
@@ -3,57 +3,59 @@ class Empty(Exception):
         Note that Empty is a subclass of python's builtin Exception class"""
     pass
 
-from collections import deque
-class ArrayQueue:
-    """ This class implements stack using deque as an Adapter Pattern.
+class ArrayStack:
+    """ This class implements stack using Python list as an Adapter Pattern.
 
         Methods to implement for realizing Stack.
-        i)  enqueue(e)
-        ii) dequeue()
+        i)  push(e)
+        ii) pop()
 
         Accessor methods.
-        i)    first ~ return top element
+        i)    top ~ return top element
         ii)   is_empty ~ returns True if the stack is empty.
         iii)  len ~ returns length of the Stack.
 
     """
 
     def __init__(self):
-        self._data = deque()
+        self._data = []
 
     def __len__(self):
         return len(self._data)
-
+    
     def is_empty(self):
         return len(self._data) == 0
-
-    def first(self):
-        if self.is_empty():
-            raise Empty('Queue is empty')
-        
-        return self._data[0]
-
-    def dequeue(self):
-        if self.is_empty():
-            raise Empty('Queue is empty')
-        
-        return self._data.popleft()
     
-    def enqueue(self, e):
+    def push(self, e):
         self._data.append(e)
 
+    def top(self):
+        if self.is_empty():
+            raise Empty("The stack is empty")
+        return self._data[ - 1]
 
-if __name__ == '__main__':
-    D = ArrayQueue()
-    D.enqueue(1)
-    D.enqueue(2)
-    D.enqueue(3)
-    D.enqueue(4)
-    D.enqueue(5)
-    D.enqueue(6)
-    D.enqueue(7)
-    D.enqueue(8)
-    print(D.dequeue())
-    print(D.first())
+    def pop(self):
+        if self.is_empty():
+            raise Empty("The stack is empty")
+        return self._data.pop()
+
     
 
+if __name__ == '__main__':
+
+    S = ArrayStack( ) # contents: [ ]
+    S.push(5) # contents: [5]
+    S.push(3) # contents: [5, 3]
+    print(len(S)) # contents: [5, 3]; outputs 2
+    print(S.pop( )) # contents: [5]; outputs 3
+    print(S.is_empty()) # contents: [5]; outputs False
+    print(S.pop( )) # contents: [ ]; outputs 5
+    print(S.is_empty()) # contents: [ ]; outputs True
+    S.push(7) # contents: [7]
+    S.push(9) # contents: [7, 9]
+    print(S.top( )) # contents: [7, 9]; outputs 9
+    S.push(4) # contents: [7, 9, 4]
+    print(len(S)) # contents: [7, 9, 4]; outputs 3
+    print(S.pop( )) # contents: [7, 9]; outputs 4
+    S.push(6) # contents: [7, 9, 6]
+    
