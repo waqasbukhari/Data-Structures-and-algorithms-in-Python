5c049ad76627dc812b8eb50d0b17e3f19ac6e3a7
ZahraShah6161 <110234218+ZahraShah6161@users.noreply.github.com>
2022-08-14 23:36:23 -0700

    runtime for tower of hanoi added

---
+++ chapter4/towerofhanoi_runtime.py
@@ -0,0 +1,36 @@
+# Recursive Python function to solve tower of hanoi
+
+def TowerOfHanoi(n , from_rod, to_rod, aux_rod):
+    
+    if n == 0:
+        return
+    else:
+        TowerOfHanoi(n-1, from_rod, aux_rod, to_rod)
+        print("Move disk",n,"from rod",from_rod,"to rod",to_rod)
+        TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
+# Driver code
+n = 5
+
+TowerOfHanoi(n, 'A', 'C', 'B')
+
+##############runtime explanation#############
+"""
+TowerOfHanoi[n]
+n = 1   ->   1 print  ->  1  = 2^0
+n = 2   ->  1 print+ TowerOfHanoi[1] +1 print   ->   1+1+1=3 = 2^2  - 1
+n = 3   ->  1 print+TowerOfHanoi[2]+1 print + TowerOfHanoi[1] +1 print
+        ->     1    +    3     +   1   + 1    + 1 = 7 = 2^3  - 1
+
+
+n = 4   ->  1 print  +  TowerOfHanoi[3]  + 1 print + TowerOfHanoi[2] + 1 print
+            +TowerOfHanoi[1] +1 print
+
+        -> 1 + 7 + 1+ 3+1+1+1 = 15 = 2^4 - 1
+
+n = n = 1 print +TowerOfHanoi[n-1]+1 print+.......+ 1 print  +  TowerOfHanoi[3]  + 1 print + TowerOfHanoi[2] + 1 print
+            +TowerOfHanoi[1] +1 print
+        1 + 2^n-1 -1 + 1 + .....+1+1+1 = 2^n - 1
+
+
+So, tower of hanoi has time complexity of O(2^n)
+"""
