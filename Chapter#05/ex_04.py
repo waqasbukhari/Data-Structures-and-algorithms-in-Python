71a69d9c6fb03a5f8525de48c05e67e257022c84
Waqas Bukhari <wbukhari@beyond.ai>
2022-09-15 17:16:35 +0300

    added 5.4

--- Chapter#05/ex_04.py
+++ Chapter#05/ex_04.py
@@ -4,3 +4,13 @@
 Modification is made to method __getitem__ of DynamicArray class. 
 Changes are enclosed within comments, specifying ex R-5.4
 """
+
+from DynamicArray import DynamicArray
+
+print("populating dynamic array. ")
+A = DynamicArray()
+for i in range(20):
+    A.append(i)
+print("array is populated. ")
+for i in range(-20,0,1):
+    print(i, A[i])
